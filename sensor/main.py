3295156499b323cea6dfec47c689d7d3a7e94da1
Adam Killeen <akilleen@fastmail.com>
2022-04-25 21:07:47 -0400

    Fix coding style

--- sensor/main.py
+++ sensor/main.py
@@ -1,5 +1,5 @@
-import network
 from time import sleep
+import network
 import sensor
 import publish
 import packages
@@ -8,15 +8,14 @@ packages.install()
 sta_if = network.WLAN(network.STA_IF)
 
 # Configure WiFi Here
-wifi_ssid = '' 
+wifi_ssid = ''
 wifi_password = ''
 
 sta_if.connect(wifi_ssid, wifi_password)
 if sta_if.active():
-      print("Wifi connected")
-      
-      
+    print("Wifi connected")
+
 while True:
-  soil_moisture_value = sensor.get_reading()
-  publish.publish(soil_moisture_value)
-  sleep(10)
+    soil_moisture_value = sensor.get_reading()
+    publish.publish(soil_moisture_value)
+    sleep(10)
